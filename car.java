9f98c71ca8c191991031da9ea7f0e1427024e60b
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-03-06 20:03:24 -0300

    Reimaginação da Classe Carros e da classe fonte

--- car.java
+++ car.java
@@ -9,16 +9,138 @@ class Car {
     double valor;
     String combustivel;
     boolean emprestado;
+    String cambio;
+    int numeroPortas;
+    int ano;
+    String placa;
 
-    
-    public Car(String nome, int velocidadeMax, String cor, String marca, boolean importado, double valor, String combustivel, boolean emprestado) {
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public int getVelocidadeMax() {
+        return velocidadeMax;
+    }
+
+    public void setVelocidadeMax(int velocidadeMax) {
         this.velocidadeMax = velocidadeMax;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
         this.cor = cor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
         this.marca = marca;
+    }
+
+    public boolean isImportado() {
+        return importado;
+    }
+
+    public void setImportado(boolean importado) {
         this.importado = importado;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
         this.valor = valor;
+    }
+
+    public String getCombustivel() {
+        return combustivel;
+    }
+
+    public void setCombustivel(String combustivel) {
         this.combustivel = combustivel;
+    }
+
+    public boolean isEmprestado() {
+        return emprestado;
+    }
+
+    public void setEmprestado(boolean emprestado) {
         this.emprestado = emprestado;
     }
+
+    public String getCambio() {
+        return cambio;
+    }
+
+    public void setCambio(String cambio) {
+        this.cambio = cambio;
+    }
+
+    public int getNumeroPortas() {
+        return numeroPortas;
+    }
+
+    public void setNumeroPortas(int numeroPortas) {
+        this.numeroPortas = numeroPortas;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    @Override
+    public String toString() {
+        return "Car [nome=" + nome + ", velocidadeMax=" + velocidadeMax + ", cor=" + cor + ", marca=" + marca
+                + ", importado=" + importado + ", valor=" + valor + ", combustivel=" + combustivel + ", emprestado="
+                + emprestado + ", cambio=" + cambio + ", numeroPortas=" + numeroPortas + ", ano=" + ano + ", placa="
+                + placa + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((placa == null) ? 0 : placa.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Car other = (Car) obj;
+        if (placa == null) {
+            if (other.placa != null)
+                return false;
+        } else if (!placa.equals(other.placa))
+            return false;
+        return true;
+    }
+
 }
--- fonte.java
+++ fonte.java
@@ -4,29 +4,6 @@ import javax.swing.JOptionPane;
 
 public class fonte {
     public static void main(String[] args) {
-        Car carro1 = new Car("Fusca", 120, "Azul", "Volkswagen", false, 20000.0, "Gasolina", false);
-        Car carro2 = new Car("Civic", 180, "Prata", "Honda", false, 50000.0, "Etanol", false);
-        Car carro3 = new Car("Mustang", 250, "Vermelho", "Ford", true, 80000.0, "Gasolina", true);
-        Car carro4 = new Car("Gol", 130, "Branco", "Volkswagen", false, 25000.0, "Flex", false);
-        Car carro5 = new Car("Corolla", 170, "Preto", "Toyota", false, 55000.0, "Gasolina", false);
-        
-        
-        String mensagem = "Temos esses carros:\n\n" +
-                "1. " + carro1.nome + "\n" +
-                "2. " + carro2.nome + "\n" +
-                "3. " + carro3.nome + "\n" +
-                "4. " + carro4.nome + "\n" +
-                "5. " + carro5.nome;
-        int opcao = JOptionPane.showConfirmDialog(null,"voce gostaria de ver nossos carros disponiveis?");
 
-        if (opcao == JOptionPane.YES_OPTION) {
-            JOptionPane.showMessageDialog(null,mensagem);
-            int opcao2 = JOptionPane.showConfirmDialog(null,"voce gostou de algum carro? ");
-            if (opcao2 == JOptionPane.YES_OPTION) {
-
-            }
-        }else{
-            System.out.println("fechando o programa.");
-        }
     }
 }
